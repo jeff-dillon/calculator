f3cde58e9a3c0ef19ff5cbaa3a0ee05abca7ae11
jeff-dillon <dillonjg@gmail.com>
2022-10-27 13:57:35 -0400

    refactor function names and add docstrings

--- script.js
+++ script.js
@@ -6,6 +6,19 @@
  * 
  */
 
+/*************************************
+ * 
+ * Operator Functions
+ * 
+**************************************/
+
+
+/**
+ * Add two numbers together.
+ * @param {Number} a - First number.
+ * @param {Number} b - Second number.
+ * @returns Result of addition.
+ */
 const add = function(a, b) {
     if(isNaN(a) || isNaN(b)) {
         throw new Error('Arguments must be numbers.');
@@ -13,6 +26,12 @@ const add = function(a, b) {
     return a + b;
 };
 
+/**
+ * Subtract one number (b) from another (a).
+ * @param {Number} a - Number ot be subtracted from.
+ * @param {Number} b - Number to subtract.
+ * @returns Result of subtraction.
+ */
 const subtract = function(a, b) {
     if(isNaN(a) || isNaN(b)) {
         throw new Error('Arguments must be numbers.');
@@ -20,6 +39,12 @@ const subtract = function(a, b) {
     return a - b;
 };
 
+/**
+ * Multiply one number by another.
+ * @param {Number} a - First number.
+ * @param {Number} b - Second number.
+ * @returns Result of multiplication.
+ */
 const multiply = function(a,b) {
     if(isNaN(a) || isNaN(b)) {
         throw new Error('Arguments must be numbers.');
@@ -27,6 +52,12 @@ const multiply = function(a,b) {
     return a * b;
 };
 
+/**
+ * Divide one number (a) by another (b).
+ * @param {Number} a - The number to be divided.
+ * @param {Number} b - The number to divide by.
+ * @returns Result of division.
+ */
 const divide = function(a, b) {
     if(isNaN(a) || isNaN(b)) {
         throw new Error('Arguments must be numbers.');
@@ -34,15 +65,62 @@ const divide = function(a, b) {
     return a / b;
 };
 
-function highlight(e){
-    e.target.classList.add('pressed');
+
+/****************************
+ *
+ * UI Code 
+ * 
+ ****************************/
+ let firstNumber = "";
+ let operator = "";
+ let secondNumber = "";
+ let displayText = "";
+
+ /**
+ * Add the highlight to the target element.
+ * @param {Element} element 
+ */
+function addHighlight(element) {
+    element.classList.add('pressed');
 }
 
-function removeHighlight(e) {
+/**
+ * Remove the highlight from the target element.
+ * @param {element} element 
+ */
+function removeHighlight(element) {
+    element.classList.remove('pressed');
+}
+ 
+/**
+ * Highlight a key on the calculator.
+ * @param {Event} e 
+ */
+function handleClick(e){
+    addHighlight(e.target);
+    if(e.target.textContent == 'AC') {
+        clearScreen();
+    } else {
+        updateScreen(e.target.textContent);
+    }
+}
+
+
+
+/**
+ * Remove the highlight from a key on the calculator.
+ * @param {Event} e 
+ */
+function handleTransition(e) {
     if(e.propertyName !== 'background-color') return;
-    e.target.classList.remove('pressed');
+    removeHighlight(e.target);
 }
 
+/**
+ * Update the calculator screen based on key press.
+ * @param {Event} e 
+ * @returns 
+ */
 function handleKeyPress(e) {
     let selectedKey = e.keyCode;
     if(e.shiftKey) {
@@ -50,16 +128,45 @@ function handleKeyPress(e) {
     }
     const key = document.querySelector(`button[data-key="${selectedKey}"`);
     if(!key) return;
-    key.classList.add('pressed');
+    addHighlight(key)
+    if(key.textContent == 'AC') {
+        clearScreen();
+    } else {
+        updateScreen(key.textContent);
+    }
+}
 
+/**
+ * Update the screen with a new character.
+ * @param {String} newChar 
+ */
+function updateScreen(newChar) {
+    console.log(newChar);
+    const operators = ["+","−","×","÷", "="];
+    const screen = document.querySelector('.screen');
+    if(operators.includes(newChar)) {
+         screen.textContent = screen.textContent + " " + newChar + " ";
+    } else {
+        screen.textContent = screen.textContent + newChar;
+    }
+}
+
+/**
+ * Clear the screen.
+ */
+function clearScreen() {
+    const screen = document.querySelector('.screen');
+    screen.textContent = "";
 }
 
 const buttons = Array.from(document.querySelectorAll('.button'));
 buttons.forEach(button => {
-    button.addEventListener('click', highlight);
-    button.addEventListener('transitionend', removeHighlight);
+    button.addEventListener('click', handleClick);
+    button.addEventListener('transitionend', handleTransition);
 });
 
 window.addEventListener('keydown', handleKeyPress);
+
+updateScreen(displayText);
 
 module.exports = {add, subtract, multiply, divide};
