3bfaa41e1026cb9db72789c4476e8db5641ff283
jeff-dillon <dillonjg@gmail.com>
2022-10-31 18:33:15 -0400

    connect UI and operator functions

--- script.js
+++ script.js
@@ -1,100 +1,114 @@
 /**
- * simple calculator application
+ * Simple calculator application
  * 
  * Requirements:
  * https://www.theodinproject.com/lessons/foundations-calculator
  * 
  */
 
+
+
+
+
 /*************************************
  * 
- * Operator Functions
+ * Calculator Operator Functions
  * 
 **************************************/
 
 
 /**
- * Add two numbers together.
- * @param {Number} a - First number.
- * @param {Number} b - Second number.
- * @returns Result of addition.
+ * Add one addend (addend1) with another addend (addend2) and return the sum.
+ * @param {Number} addend1 - First number.
+ * @param {Number} addend2 - Second number.
+ * @returns Sum resulting from addition.
  */
- const add = function(a, b) {
-    if(isNaN(a) || isNaN(b)) {
+ const add = function(addend1, addend2) {
+    if(isNaN(addend1) || isNaN(addend2)) {
         throw new Error('Arguments must be numbers.');
     }
-    return a + b;
+    return addend1 + addend2;
 };
 
 /**
- * Subtract one number (b) from another (a).
- * @param {Number} a - Number ot be subtracted from.
- * @param {Number} b - Number to subtract.
- * @returns Result of subtraction.
+ * Subtract the subtrahend (b) from the minuhend (a) and return the difference.
+ * @param {Number} minuend - The minuend.
+ * @param {Number} subtrahend - The subtrahend.
+ * @returns Difference resuting from subtraction.
  */
-const subtract = function(a, b) {
-    if(isNaN(a) || isNaN(b)) {
+const subtract = function(minuend, subtrahend) {
+    if(isNaN(minuend) || isNaN(subtrahend)) {
         throw new Error('Arguments must be numbers.');
     }
-    return a - b;
+    return minuend - subtrahend;
 };
 
 /**
- * Multiply one number by another.
- * @param {Number} a - First number.
- * @param {Number} b - Second number.
- * @returns Result of multiplication.
+ * Multiply the first factor (a) by the second factor (b) and return the product.
+ * @param {Number} factor1 - First factor.
+ * @param {Number} factor2 - Second factor.
+ * @returns Product resulting from multiplication.
  */
-const multiply = function(a,b) {
-    if(isNaN(a) || isNaN(b)) {
+const multiply = function(factor1,factor2) {
+    if(isNaN(factor1) || isNaN(factor2)) {
         throw new Error('Arguments must be numbers.');
     }
-    return a * b;
+    return factor1 * factor2;
 };
 
 /**
- * Divide one number (a) by another (b).
- * @param {Number} a - The number to be divided.
- * @param {Number} b - The number to divide by.
- * @returns Result of division.
+ * Divide the dividend (a) by the divisor (b) and return the quotient.
+ * @param {Number} dividend - The dividend.
+ * @param {Number} divisor - The divisor.
+ * @returns Quotient resulting from division.
  */
-const divide = function(a, b) {
-    if(isNaN(a) || isNaN(b)) {
+const divide = function(dividend, divisor) {
+    if(isNaN(dividend) || isNaN(divisor)) {
         throw new Error('Arguments must be numbers.');
     }
-    return a / b;
+    return dividend / divisor;
 };
 
-const operate = function(operator, a, b) {
+/**
+ * Wrapper function for add, subtract, multiply, and divide.
+ * @param {*} operator 
+ * @param {*} number1 
+ * @param {*} number2 
+ * @returns Result of the operation rounded to max 4 decimals if the result is not an integer.
+ */
+const operate = function(operator, number1, number2) {
     let returnValue = 0;
     switch(operator) {
         case '+':
-            returnValue = add(a, b);
+            returnValue = add(number1, number2);
             break;
         case '-':
-            returnValue = subtract(a, b);
+            returnValue = subtract(number1, number2);
             break;
         case '*':
-            returnValue = multiply(a, b);
+            returnValue = multiply(number1, number2);
             break;
         case '/':
-            returnValue = divide(a, b);
+            returnValue = divide(number1, number2);
             break;
+    }
+    if(!Number.isInteger(returnValue)) {
+        returnValue = Number(returnValue.toFixed(4));
     }
     return returnValue;
 
 };
 
 
+
+
+
 /****************************
  *
- * UI Code 
+ * UI Functions 
  * 
  ****************************/
- let firstNumber = "";
- let operator = "";
- let secondNumber = "";
- let displayText = "";
+
 
  /**
  * Add the highlight to the target element.
@@ -111,24 +125,60 @@ function addHighlight(element) {
 function removeHighlight(element) {
     element.classList.remove('pressed');
 }
- 
+
 /**
- * Highlight a key on the calculator.
+ * Add the highlight to the selected Operator button.
+ * @param {Element} element - the selected Operator button.
+ */
+function addHighlightOperator(element) {
+    element.classList.add('pressed-operator');
+}
+
+/**
+ * Remove the highlight from all Operator buttons
+ */
+function removeHighlightOperator() {
+    const buttons = Array.from(document.querySelectorAll('.operator'));
+    buttons.forEach(button => {
+        button.classList.remove('pressed-operator');
+    });
+}
+
+/**
+ * Update the screen with a new number.
+ * @param {String} newNumber - the number to be displayed. 
+ */
+ function updateScreen(newNumber) {
+    const screen = document.querySelector('.screen');
+    screen.textContent = newNumber;
+}
+
+function updateEquation(newEquation) {
+    const equation = document.querySelector('.equation');
+    equation.textContent = newEquation;
+}
+
+/**
+ * Clear the screen.
+ */
+function clearScreen() {
+    const screen = document.querySelector('.screen');
+    screen.textContent = "0";
+    const equation = document.querySelector('.equation');
+    equation.textContent = '';
+}
+
+/**
+ * Event handler. Handle a button click event.
  * @param {Event} e 
  */
-function handleClick(e){
-    addHighlight(e.target);
-    if(e.target.textContent == 'AC') {
-        clearScreen();
-    } else {
-        updateScreen(e.target.textContent);
-    }
+ function handleClick(e){
+    calculatorController(e.target);
 }
 
 
-
 /**
- * Remove the highlight from a key on the calculator.
+ * Event handler. Remove the highlight from a key on the calculator.
  * @param {Event} e 
  */
 function handleTransition(e) {
@@ -137,47 +187,178 @@ function handleTransition(e) {
 }
 
 /**
- * Update the calculator screen based on key press.
+ * Event handler. Handle a key press event.
  * @param {Event} e 
  * @returns 
  */
 function handleKeyPress(e) {
-    let selectedKey = e.keyCode;
+    let selectedKeyCode = e.keyCode;
+
     if(e.shiftKey) {
-        selectedKey = "shift-" + e.keyCode;
+        selectedKeyCode = "shift-" + e.keyCode;
     }
-    const key = document.querySelector(`button[data-key="${selectedKey}"`);
-    if(!key) return;
-    addHighlight(key)
-    if(key.textContent == 'AC') {
+    const selectedButton = document.querySelector(`button[data-key="${selectedKeyCode}"`);
+    if(!selectedButton) return;
+    calculatorController(selectedButton);
+    
+}
+
+
+
+
+
+/****************************************
+ * 
+ * Calculator Logic Functions
+ * 
+ ****************************************/
+
+
+// The global calc object holds and operates on the state of the calculator.
+const calc = {
+    firstNumber: 0,
+    secondNumber: 0,
+    selectedOperator: '',
+    currentNumber: 'first',
+    isDecimal: false,
+    calculationResult: 0,
+    calculationString: '',
+
+    reset: function () {
+        this.firstNumber = 0;
+        this.secondNumber = 0;
+        this.selectedOperator = "";
+        this.currentNumber = 'first';
+        this.isDecimal = false;
+        this.calculationResult = 0;
+    },
+
+    evaluateExpression: function () {
+        this.calculationResult = operate(this.selectedOperator, this.firstNumber, this.secondNumber);
+        this.calculationString = this.toString();
+        this.firstNumber = this.calculationResult;
+        this.currentNumber = 'first';
+        this.selectedOperator = "";
+        this.secondNumber = 0;
+    },
+
+    toggleDecimal: function () {
+        if(this.isDecimal) {
+            this.isDecimal = false;
+        } else {
+            this.isDecimal = true;
+        }
+    },
+
+    getCurrentNumber: function() {
+        if(this.currentNumber == 'first') {
+            return this.firstNumber;
+        } else {
+            return this.secondNumber;
+        }
+    }, 
+
+    setCurrentNumber: function(num) {
+        if(this.currentNumber == 'first') {
+            this.firstNumber = num;
+        } else {
+            this.secondNumber = num;
+        }
+    },
+
+    changeSign: function () {
+        if(this.currentNumber == 'first') {
+            this.firstNumber = this.firstNumber * -1;
+        } else {
+            this.secondNumber = this.secondNumber * -1;
+        }
+    },
+
+    evaluateOperator: function(o) {
+        if(this.selectedOperator == "") {
+            this.currentNumber = 'second';
+            this.selectedOperator = o;
+        }
+    },
+
+    evaluateNumber: function(num) {
+        if(this.currentNumber == 'first' && this.isDecimal) {
+            this.firstNumber = Number(this.firstNumber.toString() + "." + num);
+            this.toggleDecimal();
+        } else if(this.currentNumber == 'first' && !this.isDecimal) {
+            this.firstNumber = Number(this.firstNumber.toString() + num);
+        } else if(this.currentNumber == 'second' && this.isDecimal) {
+            this.secondNumber = Number(this.secondNumber.toString() + "." + num);
+            this.toggleDecimal();
+        } else {
+            this.secondNumber = Number(this.secondNumber.toString() + num);
+        }
+    },
+
+    toString: function() {
+        if(this.selectedOperator == "") {
+            return "";
+        } else {
+            return this.firstNumber + " " + this.selectedOperator + " " + this.secondNumber + " = " + this.calculationResult;
+        }
+    }
+};
+
+
+
+/**
+ * Main logic for the calculator. Performs functions depending on which button
+ * was pressed. This ties the View (UI) to the Model (calc object).
+ * @param {Element} selectedButton 
+ */
+function calculatorController(selectedButton) {
+    
+    const numbers = ['1','2','3','4','5','6','7','8','9','0'];
+    const operators = ["+","−","×","÷"];
+
+    buttonText = selectedButton.textContent;
+
+    if(buttonText == 'AC') {
+        calc.reset();
         clearScreen();
-    } else {
-        updateScreen(key.textContent);
+        addHighlight(selectedButton);
+        removeHighlightOperator(selectedButton);
+    } else if(buttonText == '+/-') { 
+        calc.changeSign();
+        updateScreen(calc.getCurrentNumber());
+        addHighlight(selectedButton);
+    } else if(buttonText == "=") { 
+        calc.evaluateExpression();
+        updateScreen(calc.getCurrentNumber());
+        updateEquation(calc.calculationString);
+        addHighlight(selectedButton);
+        removeHighlightOperator();
+    } else if(buttonText == ".") {
+        calc.toggleDecimal();
+        addHighlight(selectedButton);
+        updateScreen(calc.getCurrentNumber());
+    }else if(numbers.includes(buttonText)) { 
+        calc.evaluateNumber(buttonText);
+        updateScreen(calc.getCurrentNumber());
+        addHighlight(selectedButton);
+    } else if(operators.includes(buttonText)) { 
+        if(calc.selectedOperator == "") {
+            addHighlightOperator(selectedButton);
+            calc.evaluateOperator(selectedButton.dataset.operator);
+        }
     }
 }
 
-/**
- * Update the screen with a new character.
- * @param {String} newChar 
- */
-function updateScreen(newChar) {
-    console.log(newChar);
-    const operators = ["+","−","×","÷", "="];
-    const screen = document.querySelector('.screen');
-    if(operators.includes(newChar)) {
-         screen.textContent = screen.textContent + " " + newChar + " ";
-    } else {
-        screen.textContent = screen.textContent + newChar;
-    }
-}
 
-/**
- * Clear the screen.
- */
-function clearScreen() {
-    const screen = document.querySelector('.screen');
-    screen.textContent = "";
-}
+
+
+
+
+/*********************************************************
+ * 
+ * Register the event handler functions with the UI
+ * 
+ **********************************************************/
 
 try {
     const buttons = Array.from(document.querySelectorAll('.button'));
@@ -188,22 +369,33 @@ try {
     
     window.addEventListener('keydown', handleKeyPress);
     
-    updateScreen(displayText);
+    updateScreen('0');
 
 } catch(err) {
+    // Hack to surpress errors when running jest automated tests.
+    // There is no "document" available when running in jest.
     if(err.name != 'ReferenceError') {
         throw new Error(err);
     }
 }
 
 
-// Module exports used for jest testing, throws ReferenceError when run in browser.
+
+
+
+/***************************************************************
+ * 
+ * Make the operator functions available for unit testing. 
+ * 
+ ****************************************************************/
+
+
 try {
     module.exports = {add, subtract, multiply, divide, operate};
 } catch(err) {
+    // Hack to surpress errors when running script in browser.
+    // There is no "module" available when running in browser.
     if(err.name != 'ReferenceError') {
         throw new Error(err);
     }
 }
-
-
